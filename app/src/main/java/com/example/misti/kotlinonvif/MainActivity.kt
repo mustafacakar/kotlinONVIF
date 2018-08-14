a9aa5223d5222d5b1fe27ac43b3da21d3d48a59e
master <mistik_cakar@hotmail.com>
2018-08-14 13:24:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/misti/kotlinonvif/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.misti.kotlinonvif
+
+import android.graphics.Region
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.SurfaceView
+import android.widget.Toast
+import com.pedro.vlc.VlcListener
+import com.pedro.vlc.VlcVideoLibrary
+import com.rvirin.onvif.onvifcamera.*
+
+class MainActivity : AppCompatActivity(), OnvifListener ,VlcListener{
+    override fun onComplete() {
+        Toast.makeText(this,"Video Yükleniyor", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onError() {
+        Toast.makeText(this,"Hata", Toast.LENGTH_LONG).show()
+    }
+
+    var vlcVideoLibrary: VlcVideoLibrary? =null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Aygıtın ip,username,pass ları burda belirtiyoruz.
+
+       currentDevice =  OnvifDevice("10.59.143.132:8080","388724","mumy");
+        currentDevice.listener = this
+        currentDevice.getServices()
+
+    }
+
+
+    override fun requestPerformed(response: OnvifResponse) {
+
+        //Bağlantı kurulduysa surfaceView e yönlendirme.
+        //VLC MediaPlayer vs. burada.
+        Log.d("INFO",response.parsingUIMessage)
+
+        if(response.request.type == OnvifRequest.Type.GetDeviceInformation){
+            currentDevice.getProfiles()
+        }else if(response.request.type == OnvifRequest.Type.GetProfiles) {
+            currentDevice.mediaProfiles.firstOrNull()?.let {
+                currentDevice.getStreamURI(it)
+            }
+        }else if(response.request.type == OnvifRequest.Type.GetStreamURI){
+
+            currentDevice.rtspURI?.let {uri ->
+
+                 val surfaceView = findViewById<SurfaceView>(R.id.surfaceView)
+                vlcVideoLibrary= VlcVideoLibrary(this,this,surfaceView)
+                vlcVideoLibrary?.play(uri)
+
+
+            }
+
+        }
+
+
+
+        /*  bağlantı dogru kuruldu mu kurulmadı mı kontrolleri burdan.
+        //region BAĞLANTI KONTROL İÇİN KONTROLLER
+        Log.d("INFO",response.parsingUIMessage)
+
+        if(response.request.type == OnvifRequest.Type.GetServices){
+            currentDevice.getDeviceInformation()
+        }else if(response.request.type == OnvifRequest.Type.GetDeviceInformation){
+            currentDevice.getProfiles()
+        }else if(response.request.type == OnvifRequest.Type.GetProfiles){
+            currentDevice.getStreamURI(currentDevice.mediaProfiles.first())
+        }
+
+        //endregion
+        */
+    }
+}
